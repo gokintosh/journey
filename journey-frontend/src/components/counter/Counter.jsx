c5fc7b689b7aea1719b284c8503bbb7b86a68200
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-12-25 16:58:27 +0100

    add decrement method

--- journey-frontend/src/components/counter/Counter.jsx
+++ journey-frontend/src/components/counter/Counter.jsx
@@ -18,6 +18,7 @@ export default class Counter extends Component{
           <CounterButton incrementmethod={this.increment}/>
           <CounterButton by={5} incrementmethod={this.increment}/>
           <CounterButton by={10} incrementmethod={this.increment}/>
+          
 
           <span className="count">{this.state.counter}</span>
           
@@ -28,9 +29,10 @@ export default class Counter extends Component{
 
     increment(by){
         // this.state.counter++;
-        this.setState({
-            counter:this.state.counter+by
-        });
+        this.setState(
+            (prevState)=>{
+                return {counter:prevState.counter+by}
+            });
         // console.log(`increment from parent- ${by}`);
     }
   }
@@ -39,26 +41,35 @@ class CounterButton extends Component
 {
     constructor(){
         super();
-        this.state={
-            counter:0
-        }
+    //     // this.state={
+    //     //     counter:0
+    //     // }
         this.increment=this.increment.bind(this);
+        this.decrement=this.decrement.bind(this);
     }
     render(){
         return(
             <div className="counterButton">
+
+            {/* props from the counter button above (by) */}
               <button onClick={this.increment}>+{this.props.by}</button>
+              <button onClick={this.decrement}>-{this.props.by}</button>
               {/* <span className="count">{this.state.counter}</span> */}
               {/* we dont need to add this */}
             </div>
           )
     }
+    decrement(){
+        this.props.incrementmethod(-(this.props.by))
+    }
 
     increment(){
         // this.state.counter++;
-        this.setState({
-            counter:this.state.counter+this.props.by
-        });
+        // this.setState({
+        //     counter:this.state.counter+this.props.by
+        // });
+
+        // also calls the parent increment method
         this.props.incrementmethod(this.props.by);
     }
 }
