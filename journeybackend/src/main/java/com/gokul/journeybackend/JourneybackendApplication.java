9c2a763979a555fbe047647f67d4b6e73f9a2cdb
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-12-30 14:17:06 +0100

    rest initiate

---
+++ journeybackend/src/main/java/com/gokul/journeybackend/JourneybackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.journeybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneybackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneybackendApplication.class, args);
+    }
+
+}
---
+++ journeybackend/src/test/java/com/gokul/journeybackend/JourneybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.journeybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneybackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
